1b4ec47f5c5a07caac150d29c5094804f3806338
Suissa <jnascimento@gmail.com>
2021-11-18 04:31:10 -0300

    Python HUMILHA JS nas Datas

--- convertendo_datas/index.js
+++ convertendo_datas/index.js
@@ -1,2 +1,8 @@
 ts = Date.now()
 console.log(ts)
+
+dateTs = new Date(ts)
+console.log(dateTs)
+// retornou com 3 hrs a mais
+// pois está no horário UTC
+// python bem melhor nissoo
--- convertendo_datas/index.py
+++ convertendo_datas/index.py
@@ -1,4 +1,9 @@
 import time
   
 ts = time.time()
-print(ts)
+print(ts * 1000)
+
+from datetime import datetime
+
+date01 = datetime.fromtimestamp(ts)
+print(date01)
