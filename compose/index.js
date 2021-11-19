f8eab4fbe4661716e95cb9cc5ea3df2e6551b14e
Suissa <jnascimento@gmail.com>
2021-11-19 13:08:55 -0300

    fazer o pipe/compose generico no PY vai ser tenso, tendo em vista q usamos o reduce praisso

--- compose/index.js
+++ compose/index.js
@@ -1,9 +1,9 @@
-const compose = ( f1, f2 ) => value => f1( f2( value ) )
+compose = ( f1, f2 ) => value => f1( f2( value ) )
 
-const add7 = ( x ) => x + 7
-const times10 = ( x ) => x * 10
+add7 = ( x ) => x + 7
+times10 = ( x ) => x * 10
 
-const runCompose = ( x ) => compose( add7, times10 )( x )
+runCompose = ( x ) => compose( add7, times10 )( x )
 
 console.log( runCompose( 5 ) ) // 57 = 5 * 10 = 50 + 7
 
