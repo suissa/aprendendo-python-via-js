51991163e8dd16c31dd6e7b939e155c53f8a114b
Suissa <jnascimento@gmail.com>
2021-11-19 14:24:07 -0300

    pipe correto

--- pipe/index.completo.py
+++ pipe/index.completo.py
@@ -1,9 +1,12 @@
 import functools
+reduce = functools.reduce
 
 add7 = lambda x: x + 7
 times10 = lambda x: x * 10
 minus20 =lambda x: x - 20
 
-fn_pipe = lambda v, f: f(v)
-ex01 = functools.reduce(fn_pipe, [add7, times10, minus20], 5)
+pipe = lambda fns, x: reduce(lambda x, f: f(x), fns, x)
+
+ex01 = pipe([add7, times10, minus20], 5)
 print(ex01) #100
+
